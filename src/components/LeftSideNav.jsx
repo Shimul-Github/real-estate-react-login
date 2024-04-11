e515aa2409a9011a35bbd6e78c528c3ec9e8d5b5
Shimul-Github <shimulganankaj@gmail.com>
2024-04-12 01:21:25 +0200

    left side with response

--- src/components/LeftSideNav.jsx
+++ src/components/LeftSideNav.jsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const LeftSideNav = () => {
+    const [properties, setProperties] = useState([])
+    useEffect(()=>{
+        fetch('/comercialData.JSON')
+        .then(res => res.json())
+        .then(data => setProperties(data))
+    },[])
     return (
-        <div>
-            <h1 className="text-2xl">Left Side</h1>
+        <div className='space-y-4'>
+            <h1 className="text-xl font-bold border-2 border-green">Commercial Properties</h1>
+            {
+                properties.map(property =>
+                     <Link className='block ml-4 text-xl'
+                      key={property.id}
+                      to={`/property/${property.id}`}>
+                        {property.name}
+                     </Link>)
+            }
         </div>
     );
 };
